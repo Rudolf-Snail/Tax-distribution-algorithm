03cb98bbe60f5ad5477e32a93b78021145aa7934
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2026-05-22 18:10:42 +0200

    Add Input.cpp

---
+++ Input.cpp
@@ -0,0 +1,124 @@
+#ifndef Input_cpp
+#define Input_cpp
+
+#include "Input.h"
+
+long double CheckAndProcessInput(const std::string &inputInstructions, const std::string &valueOutOfRangeMessage, const std::string &conversionUnsuccesfulMessage, const std::string &valueOutOfAcceptedRangeMessage, const long double minimumValueInclusive, const long double maximumValueInclusive, const std::string &emptyInputMessage, const bool canGetMultipleValues, std::unique_ptr<std::vector<long double>> *vectorOfMultipleValues, const std::string &vectorEmptyMessage)
+{
+    CreateVectorIfEmptyAndMultipleValuesIsTrue(canGetMultipleValues, *vectorOfMultipleValues);
+
+    std::string input = "";
+    long double value = 0;
+
+    char *afterLastInterpretedCharacter;
+
+    while (true)
+    {
+        std::cout << inputInstructions;
+
+        std::getline(std::cin, input);
+
+        switch (EmptyInput(canGetMultipleValues, input, *vectorOfMultipleValues, vectorEmptyMessage, value, emptyInputMessage))
+        {
+        case NextAction::endAndReturn:
+            return value;
+        case NextAction::goToNextIteration:
+            continue;
+        case NextAction::goOn:
+            break;
+        }
+
+        afterLastInterpretedCharacter = 0;
+
+        value = std::strtold(input.c_str(), &afterLastInterpretedCharacter);
+
+        switch (ConversionSuccessful(value, valueOutOfRangeMessage, afterLastInterpretedCharacter, input, conversionUnsuccesfulMessage, valueOutOfAcceptedRangeMessage, minimumValueInclusive, maximumValueInclusive))
+        {
+        case NextAction::goToNextIteration:
+            continue;
+        case NextAction::goOn:
+            break;
+        }
+
+        switch (ProcessCorrectValue(canGetMultipleValues, *vectorOfMultipleValues, value))
+        {
+        case NextAction::goToNextIteration:
+            continue;
+        case NextAction::endAndReturn:
+            return value;
+        }
+    }
+}
+
+void CreateVectorIfEmptyAndMultipleValuesIsTrue(const bool canGetMultipleValues, std::unique_ptr<std::vector<long double>> &vectorOfMultipleValues)
+{
+    if (canGetMultipleValues && vectorOfMultipleValues == nullptr)
+    {
+        vectorOfMultipleValues = std::make_unique<std::vector<long double>>();
+    }
+}
+
+NextAction ProcessCorrectValue(const bool canGetMultipleValues, std::unique_ptr<std::vector<long double>> &vectorOfMultipleValues, const long double value)
+{
+    if (canGetMultipleValues)
+    {
+        (*vectorOfMultipleValues).push_back(value);
+
+        return NextAction::goToNextIteration;
+    }
+    else
+    {
+        return NextAction::endAndReturn;
+    }
+}
+
+constexpr NextAction ConversionSuccessful(const long double value, const std::string &valueOutOfRangeMessage, const char *afterLastInterpretedCharacter, const std::string &input, const std::string &conversionUnsuccesfulMessage, const std::string &valueOutOfAcceptedRangeMessage, const long double minimumValueInclusive, const long double maximumValueInclusive)
+{
+    if (value == HUGE_VALL)
+    {
+        std::cout << valueOutOfRangeMessage;
+
+        return NextAction::goToNextIteration;
+    }
+    else if (value == 0 && afterLastInterpretedCharacter == input)
+    {
+        std::cout << conversionUnsuccesfulMessage;
+
+        return NextAction::goToNextIteration;
+    }
+    else if (value < minimumValueInclusive || value > maximumValueInclusive)
+    {
+        std::cout << valueOutOfAcceptedRangeMessage;
+
+        return NextAction::goToNextIteration;
+    }
+    else
+    {
+        return NextAction::goOn;
+    }
+}
+
+constexpr NextAction EmptyInput(const bool canGetMultipleValues, const std::string &input, const std::unique_ptr<std::vector<long double>> &vectorOfMultipleValues, const std::string &vectorEmptyMessage, const long double value, const std::string &emptyInputMessage)
+{
+    if (canGetMultipleValues && input == "")
+    {
+        if ((*vectorOfMultipleValues).empty())
+        {
+            std::cout << vectorEmptyMessage;
+
+            return NextAction::goToNextIteration;
+        }
+
+        return NextAction::endAndReturn;
+    }
+    else if (input == "")
+    {
+        std::cout << emptyInputMessage;
+
+        return NextAction::goToNextIteration;
+    }
+
+    return NextAction::goOn;
+}
+
+#endif
