fea9862418c23cf2f68cad4eee974e455f310dff
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2026-05-24 17:00:11 +0200

    Add licensing information to the program

--- TaxDistributorClass.h
+++ TaxDistributorClass.h
@@ -1,3 +1,11 @@
+// Tax distribution algorithm — a tax distributing program
+// Copyright (C) 2026  Martin Herchel
+
+// This file is part of Tax distribution algorithm.
+// Tax distribution algorithm is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// Tax distribution algorithm is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with Tax distribution algorithm. If not, see <https://www.gnu.org/licenses/>. 
+
 #ifndef TaxDistributorClass_h
 #define TaxDistributorClass_h
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+// Tax distribution algorithm — a tax distributing program
+// Copyright (C) 2026  Martin Herchel
+
+// This file is part of Tax distribution algorithm.
+// Tax distribution algorithm is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// Tax distribution algorithm is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with Tax distribution algorithm. If not, see <https://www.gnu.org/licenses/>. 
+
 #include "Input.cpp"
 
 #include <format>
 
 int main()
 {
+    std::cout << "Tax distribution algorithm Copyright (C) 2026  Martin Herchel.\n" <<
+                "This program comes with ABSOLUTELY NO WARRANTY; details available in LICENSE.txt file.\n" <<
+                "This is free software, and you are welcome to redistribute it under certain conditions; specified in the LICENSE.txt file.\n\n";
+
     auto incomes = std::make_unique<std::vector<long double>>();
 
     const std::string inputInstructions1 = "Enter number with the value of income to add and press Enter to add it to the list of incomes; press Enter with no number or value present to stop adding incomes: ";
